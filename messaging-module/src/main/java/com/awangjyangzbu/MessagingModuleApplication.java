8b9364aa4eaf6d73a7d3cc5bb5eec6cd52f40e5e
Allan W <10alan0712@gmail.com>
2024-03-05 05:04:59 -0800

    Initial commit

---
+++ messaging-module/src/main/java/com/awangjyangzbu/MessagingModuleApplication.java
@@ -0,0 +1,13 @@
+package com.awangjyangzbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingModuleApplication.class, args);
+    }
+
+}
